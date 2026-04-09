2358dff8fea230174fa8964161fe2a21c6082590
Javier-GarciaP <josejaviergarciap123@gmail.com>
2026-04-09 03:04:24 -0400

    Ajuste de estilo de mapa y limpieza

--- src/components/map/CampusMap.jsx
+++ src/components/map/CampusMap.jsx
@@ -536,8 +536,8 @@ const CampusMap = forwardRef(function CampusMap({
         zoomControl={false}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png"
           maxZoom={20}
           maxNativeZoom={19}
         />
